c1fbbaaf699d01a5c128d02069ddd18e14fdde70
Allan Rojas <allanjose91@gmail.com>
2015-05-29 20:34:14 -0500

    V 1.8

--- Prototipo/src/util/XmlFormatter.java
+++ Prototipo/src/util/XmlFormatter.java
@@ -14,7 +14,13 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- */
+ *
+
+// Esta es la clase mas fake de todas 
+// Haha no tengo idea de como manejar bien los XML 
+// Espero que me ayuden :P
+
+
 package util;
 
 /**
